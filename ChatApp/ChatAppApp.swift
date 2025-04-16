abb8a832f971483817e37dee75511ed4a68dbdfb
Michal Ukropec <ukropec.michal.jr@gmail.com>
2025-04-16 19:54:31 +0200

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Michal Ukropec on 16/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
